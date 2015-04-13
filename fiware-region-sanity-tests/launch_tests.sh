ac45fc8670f7100d476044bce003678f2dca3795
Pablo Rodríguez <pablo.rodriguezarchilla@telefonica.com>
2015-04-13 11:29:07 +0200

    Add support for restricting launch_tests to specific regions

--- fiware-region-sanity-tests/launch_tests.sh
+++ fiware-region-sanity-tests/launch_tests.sh
@@ -20,25 +20,33 @@
 # Launch testcase execution and generate reports
 #
 # Usage:
-#     $0 [--verbose]
+#     $0 [--verbose] [region ...]
 #     $0 --help
 #
 # Options:
 #     -v, --verbose	enable logging with verbose details
 #     -h, --help	show this help message
 #
+# Regions:
+#     Given by existing tests/regions/test_{region_name}.py files
+#
 
 OPTS='v(verbose)h(help)'
 NAME=$(basename $0)
 
 # nosetests options
-TESTS=tests/regions
+TESTS=
 NOSEOPTS="--logging-filter=TestCase --logging-level=ERROR"
+
+# Available regions
+REGIONS=$(cd tests/regions; ls test_*.py | sed 's/test_\(.*\).py/\1/g')
+REGIONS_PATTERN='^\('$(echo $REGIONS | sed 's/ /\\|/g')'\)$'
 
 # Process command line
 OPTERR=
 OPTSTR=$(echo :-:$OPTS | sed 's/([a-zA-Z0-9]*)//g')
-OPTHLP=$(sed -n '20,/^$/ { s/$0/'$NAME'/; s/^#[ ]\?//p }' $0)
+OPTHLP=$(sed -n '20,/^$/ { s/$0/'$NAME'/; s/^#[ ]\?//p }' $0 | head -n -2;
+	for i in $REGIONS ""; do printf "    %s\n" $i; done)
 while getopts $OPTSTR OPT; do while [ -z "$OPTERR" ]; do
 case $OPT in
 'v')	NOSEOPTS="$NOSEOPTS --logging-level=DEBUG";;
@@ -57,7 +65,11 @@ case $OPT in
 	continue;;
 esac; break; done; done
 shift $(expr $OPTIND - 1)
-[ -z "$OPTERR" -a -n "$*" ] && OPTERR="Too many arguments"
+while [ -z "$OPTERR" -a -n "$1" ]; do
+	REGION_NAME=$(expr "$1" : "$REGIONS_PATTERN") && shift
+	[ -z "$REGION_NAME" ] && OPTERR="Invalid region '$1'"
+	TESTS="$TESTS tests.regions.test_$REGION_NAME"
+done
 [ -n "$OPTERR" ] && {
 	[ "$OPTERR" != "$OPTHLP" ] && OPTERR="${OPTERR}\nTry \`$NAME --help'"
 	TAB=4; LEN=$(echo "$OPTERR" | awk -F'\t' '/ .+\t/ {print $1}' | wc -L)
@@ -65,6 +77,7 @@ shift $(expr $OPTIND - 1)
 	printf "$OPTERR" | tr -s '\t' | expand -t$TABSTOPS | fmt -$WIDTH -s 1>&2
 	exit 1
 }
+TESTS=${TESTS:-tests/regions}
 
 # Main
 nosetests $TESTS $NOSEOPTS -v --exe \
