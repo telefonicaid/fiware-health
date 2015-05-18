66936c4d5dd700b688ac1d0eea11d4801875c788
Jesus Perez <jesuspg@tid.es>
2015-05-18 10:51:54 +0200

    improve css style improve UX about sign in button change template for use common header

--- dashboard/lib/public/javascripts/dashboard.js
+++ dashboard/lib/public/javascripts/dashboard.js
@@ -1,7 +1,34 @@
+function calc_height() {
+    var B = document.body,
+    H = document.documentElement,
+    height;
+
+if (typeof document.height !== 'undefined') {
+    height = document.height; // For webkit browsers
+} else {
+    height = Math.max( B.scrollHeight, B.offsetHeight,H.clientHeight, H.scrollHeight, H.offsetHeight );
+}
+
+    console.log("resize1 "+height);
+    window.parent.document.getElementById('iframe-container').style.height = height + 'px';
+
+}
+
 function displaySections() {
-    Array.prototype.forEach.call(document.querySelectorAll('h2, h3, h4'), function (el) {
+    calc_height();
+
+    Array.prototype.forEach.call(document.querySelectorAll('h3, h4'), function (el) {
         el.addEventListener('click', function () {
-            el.nextElementSibling.style.display = document.defaultView.getComputedStyle(el.nextElementSibling).display == 'none' ? 'block' : 'none';
+
+            if (document.defaultView.getComputedStyle(el.nextElementSibling).display == 'none') {
+                el.nextElementSibling.style.display = 'block';
+            } else {
+                el.nextElementSibling.style.display = 'none';
+            }
+            calc_height();
+
+
+
         })
     })
 }
@@ -9,19 +36,21 @@ function displaySections() {
 
 var strWindowFeatures = "resizable=yes,scrollbars=yes,status=yes";
 function openFailureDetailsInNewWindow(anchor_tag) {
+
     var html = document.getElementById("html_failure_details").innerHTML;
     var myWindow = window.open(anchor_tag, '_self', strWindowFeatures);
     var doc = myWindow.document;
     doc.open();
     doc.write(html);
     doc.close();
+    calc_height();
 }
 
 function changeTitle(id) {
     var element = document.getElementById(id);
     var subtitle = element.innerText || element.textContent;
     var n = subtitle.search(".TestSuite");
-    var new_subtitle = "SANITY CHECK STATUS | " + capitalizeFirstLetter(subtitle.substring(48, n)) + subtitle.substring(n + 10);
+    var new_subtitle = capitalizeFirstLetter(subtitle.substring(26, n)) + subtitle.substring(n + 10);
     if (element.textContent == undefined) {
         document.getElementById(id).innerText = new_subtitle;
     } else {
@@ -50,6 +79,14 @@ function refresh(button, region) {
     }
 
     window.open('/refresh?region=' + region, '_self');
+}
 
+
+
+function loadReport(filename) {
+
+  document.getElementById("box1").style.display="none";
+  var el=document.getElementById("frameContainer");
+  el.outerHTML="<iframe id='iframe-container' src='/report/"+filename+"' scrolling='no'></iframe>";
 
 }
