535607c8b1f9b1111568b3a551b75c97e2790696
Pablo Rodríguez <pablo.rodriguezarchilla@telefonica.com>
2015-09-22 16:37:43 +0200

    Add operation to get the list of shared networks and select the one to use in E2E tests

--- fiware-region-sanity-tests/tests/fiware_region_with_networks_tests.py
+++ fiware-region-sanity-tests/tests/fiware_region_with_networks_tests.py
@@ -162,10 +162,18 @@ class FiwareRegionWithNetworkTest(FiwareRegionsBaseTests):
         HELPER. Finds and returns the shared network name of current region
         :return: Shared network name
         """
-        shared_network_name = TEST_SHARED_NET_DEFAULT
+        # get from settings the name of the shared network to lookup
+        lookup_network_name = TEST_SHARED_NET_DEFAULT
         shared_network_conf = self.conf[PROPERTIES_CONFIG_REGION].get(PROPERTIES_CONFIG_REGION_SHARED_NET)
         if shared_network_conf:
-            shared_network_name = shared_network_conf.get(self.region_name, TEST_SHARED_NET_DEFAULT)
+            lookup_network_name = shared_network_conf.get(self.region_name, TEST_SHARED_NET_DEFAULT)
+
+        # find the network in the list of existing shared networks
+        lookup_network_list = self.neutron_operations.find_networks(name=lookup_network_name,
+                                                                    shared=True,
+                                                                    router_external=False)
+        shared_network_name = lookup_network_list[0]['name'] if lookup_network_list else None
+        self.assertIsNotNone(shared_network_name, "No shared network %s found" % lookup_network_name)
 
         return shared_network_name
 
