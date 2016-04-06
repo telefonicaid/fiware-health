a1e7812ac7fe13dae537f9becb9df3e0bc558e92
Fernando Lopez <fernando.lopezaguilar@telefonica.com>
2016-04-06 15:59:38 +0200

    Fix: Usr:Add Vicenza node to the Sanity Checks.

---
+++ fiware-region-sanity-tests/tests/regions/test_vicenza.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2015 Telefónica Investigación y Desarrollo, S.A.U
+#
+# This file is part of FIWARE project.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+#
+# You may obtain a copy of the License at:
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# For those usages not covered by the Apache version 2.0 License please
+# contact with opensource@tid.es
+
+__author__ = 'fla'
+
+
+from tests import fiware_region_with_networks_tests, fiware_region_object_storage_tests
+
+
+class TestSuite(fiware_region_with_networks_tests.FiwareRegionWithNetworkTest,
+                fiware_region_object_storage_tests.FiwareRegionsObjectStorageTests):
+    region_name = "Vicenza"
