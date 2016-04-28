5abf3d3b4d4eaaa2df31ce19835b617c580d9e63
pratid <pablo.rodriguezarchilla@telefonica.com>
2016-04-28 18:01:49 +0200

    Exit with error code when settings could not be loaded

--- fiware-region-sanity-tests/tests/run.py
+++ fiware-region-sanity-tests/tests/run.py
@@ -53,6 +53,7 @@ from tests.fiware_region_object_storage_tests import FiwareRegionsObjectStorageT
 from commons.constants import DEFAULT_SETTINGS_FILE, DEFAULT_LOGGING_CONF, \
     PROPERTIES_CONFIG_REGION, PROPERTIES_CONFIG_REGION_SHARED_NET, PROPERTIES_CONFIG_SWIFT_ENABLED
 
+import sys
 import os.path
 import argparse
 import json
@@ -73,7 +74,8 @@ class RegionTestsLoader(nose.loader.TestLoader):
             try:
                 self.conf = json.load(settings)
             except Exception as e:
-                assert False, "Error parsing settings file '{}': {}".format(self.settings_file, e)
+                print "Error parsing settings file '{}': {}".format(self.settings_file, e)
+                sys.exit(-1)
 
             region_conf = self.conf[PROPERTIES_CONFIG_REGION]
             region_list = [str(region) for region in region_conf.keys()]
