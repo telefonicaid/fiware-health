41c1ce3fd259b710108639660a43711ead2c4343
visitor <henar.munozfrutos@telefonica.com>
2016-05-10 12:41:25 +0200

    update files

--- fiware-region-sanity-tests/docker/start.sh
+++ fiware-region-sanity-tests/docker/start.sh
@@ -1,3 +1,16 @@
+export OS_PROJECT_DOMAIN_NAME=default
+export OS_USER_DOMAIN_NAME=default
+export OS_IDENTITY_API_VERSION=3
+export JENKINS_HOME=.
+export JENKINS_URL=.
+export JENKINS_JOB=.
+export FIHEALTH_WORKSPACE=.
+export FIHEALTH_HTDOCS=.
+export FIHEALTH_ADAPTER_URL=.
+export FIHEALTH_CB_URL=.
+export JENKINS_USER=.
+export JENKINS_PASSWORD=.
+sleep 12000
 echo ${KEY} >> key.pem
 chmod 0600 key.pem
-ssh -i key.pem ubuntu@${VMIP} -fnN -R0:8081:0:8081 &
+ssh -i key.pem -o "StrictHostKeyChecking no" ubuntu@${VMIP} -fnN -R0:8081:0:8081 &
