435965ab72352674477c998033e85bfb5b0f7b58
visitor <henar.munozfrutos@telefonica.com>
2016-05-16 14:27:51 +0200

    update

--- fiware-region-sanity-tests/tests/fiware_region_with_networks_tests.py
+++ fiware-region-sanity-tests/tests/fiware_region_with_networks_tests.py
@@ -583,6 +583,7 @@ class FiwareRegionWithNetworkTest(FiwareRegionsBaseTests):
 
         result = client.connect_and_wait_for_phonehome_signal(PHONEHOME_DBUS_NAME, PHONEHOME_DBUS_OBJECT_METADATA_PATH,
                                                               PHONEHOME_METADATA_SIGNAL, expected_instance_name)
+
         # First, check that the DBus is registered on the system
         self.assertNotEqual(result, False, "PhoneHome bus or object not found. Please check the PhoneHome services.")
 
